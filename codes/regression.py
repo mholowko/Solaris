9d2d3a4ab4973ca7b57b1b1268668a7b30b7e77e
Mengyanz <u6015325@anu.edu.au>
2020-02-10 20:37:15 +1100

    first round result analysis.

--- codes/regression.py
+++ codes/regression.py
@@ -34,7 +34,7 @@ class Regression():
         labels array (num_samples, )
         second column of data, each element is a float/int
     """
-    def __init__(self, model, data, embedding_method = None, 
+    def __init__(self, model, data, data_test = None, embedding_method = None, 
                  precomputed_kernel = None):
         """
         Paramters:
@@ -51,13 +51,25 @@ class Regression():
         """
         self.model = model
         
-        if embedding_method is not None:
-            self.my_env = Rewards_env(data, embedding_method)
-            self.X = self.my_env.embedded
+        if data_test is None:
+            if embedding_method is not None:
+                self.my_env = Rewards_env(data, embedding_method)
+                self.X = self.my_env.embedded
+            else:
+                self.X = data[:, 0]
+            self.Y = data[:, 1]
+            self.split_data()
         else:
-            self.X = data[:, 0]
-        self.Y = data[:, 1]
-        self.split_data()
+            if embedding_method is not None:
+                self.my_env_train = Rewards_env(data, embedding_method)
+                self.X_train = self.my_env_train.embedded
+                self.my_env_test = Rewards_env(data_test, embedding_method)
+                self.X_test = self.my_env_test.embedded
+            else:
+                self.X_train = data[:, 0]
+                self.X_test = data_test[:, 0]
+            self.Y_train = data[:, 1]
+            self.Y_test = data_test[:,1]
 
         if self.model.kernel == 'precomputed':
             self.precompute_kernel(precomputed_kernel)
@@ -92,7 +104,7 @@ class Regression():
         self.train_predict = self.model.predict(self.X_train)
         self.test_predict = self.model.predict(self.X_test)
 
-    def evaluate(self, cross_val_flag = True, print_flag = True, plot_flag = True, k = 10, metric = 'neg_mean_squared_error'):
+    def evaluate(self, cross_val_flag = True, print_flag = True, plot_flag = True, k = 10, metric = 'NRMSE'):
         """Evaluate.
         Calculate RMSE score for both training and testing datasets.
         """
@@ -101,9 +113,13 @@ class Regression():
             scores = cross_val_score(self.model, self.X, self.Y, cv = cv, scoring= metric)
             scores = np.sqrt(-scores)
 
-        # use the normalised root mean square error
-        train_score = np.sqrt(mean_squared_error(self.Y_train, self.train_predict))/(max(self.Y_train) - min(self.Y_test))
-        test_score = np.sqrt(mean_squared_error(self.Y_test, self.test_predict))/(max(self.Y_train) - min(self.Y_test))
+        if metric is 'NRMSE':
+            # use the normalised root mean square error
+            train_score = np.sqrt(mean_squared_error(self.Y_train, self.train_predict))/(max(self.Y_train) - min(self.Y_test))
+            test_score = np.sqrt(mean_squared_error(self.Y_test, self.test_predict))/(max(self.Y_train) - min(self.Y_test))
+        elif metric is 'r2_score':
+            train_score = r2_score(self.Y_train, self.train_predict)
+            test_score =  r2_score(self.Y_test, self.test_predict)
 
         if print_flag:
             print('Model: ', str(self.model))
@@ -111,8 +127,8 @@ class Regression():
                 print(scores)
                 print("RMSE : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
             else:
-                print('Train RMSE: ', train_score)
-                print('Test RMSE: ', test_score)
+                print('Train '+metric+ ': '+ str(train_score))
+                print('Test '+metric+ ': '+ str(test_score))
         if plot_flag:
             self.plot()
         
@@ -124,8 +140,8 @@ class Regression():
         plt.figure() 
         plt.plot(self.test_predict, self.Y_test, 'r.', label = 'test')
         plt.plot(self.train_predict, self.Y_train, 'b.', label = 'train')
-        max_label = max(self.Y)
-        min_label = min(self.Y)
+        max_label = max(list(self.Y_train) + list(self.Y_test))
+        min_label = min(list(self.Y_train) + list(self.Y_test))
         plt.plot([min_label,max_label], [min_label,max_label], '--')
         plt.plot([min_label,max_label], [max_label/2.0,max_label/2.0], 'k--')
         plt.plot([max_label/2.0,max_label/2.0], [min_label,max_label], 'k--')
