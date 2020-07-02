5867f1a530e606d81da3133d64b60b3d19907794
Mengyanz <u6015325@anu.edu.au>
2020-07-02 13:57:01 +1000

    kernel matrix with raw label.

--- codes/regression_copy.py
+++ codes/regression_copy.py
@@ -12,19 +12,13 @@ from sklearn.model_selection import train_test_split, cross_val_score, ShuffleSp
 from sklearn.metrics import r2_score, mean_squared_error
 
 from codes.environment import Rewards_env
-from codes.kernels import spectrum_kernel, mixed_spectrum_kernel, WD_kernel, WD_shift_kernel
-
-KERNEL_TYPE = {'spectrum': spectrum_kernel,
-               'mixed_spectrum': mixed_spectrum_kernel,
-               'WD': WD_kernel,
-               'WD_shift': WD_shift_kernel
-            }
+from codes.kernels_for_GPK import Spectrum_Kernel, Sum_Spectrum_Kernel
 
 # rewrite the regression code, including
 # 1. regression with passed training/testing data, return predicted mean and std,
 #       where the returned predicted mean and std can be used as the input of UCB algorithms
 # 2. regression with the whole dataset, with splitting data method of function.
-# 3. cross validation regression, to select hyparameters
+# 3. cross validation regression, to select hyperparameters
 
 # For all classes,
 # take the predictor, embedding, kernel as input
@@ -43,7 +37,7 @@ class Regression():
         labels array (num_samples, )
         second column of data, each element is a float/int
     """
-    def __init__(self, model, kernel, data, data_split=None, embedding_method=None, **kwargs):
+    def __init__(self, model, kernel, data, data_split=None, embedding_method='label', **kwargs):
         """
         Parameters:
         ------------------------------------------------------
@@ -60,54 +54,58 @@ class Regression():
         embedding_method: string, default is None
             if None, no embedding is performed and set X to the first column of data
         """
-        self.model = model
+        
         self.kernel = kernel
-        self.data = data
+        self.env = Rewards_env(data, embedding_method=embedding_method)
+        self.X = self.env.embedded
+        self.Y = data[:,1]
         self.data_split = data_split
 
         # hyperparameters
-        self.l_list = kwargs.get('l_list', [3]) # default kmer (k=3)
+        self.l_list = kwargs.get('l_list', [2,3,4,5,6]) # default kmer (k=3)
+        self.alpha = kwargs.get('alpha', 0.05)
+
+        self.model = model(kernel = self.kernel(l_list = self.l_list), alpha = self.alpha)
         # TODO: add others
 
     def split_data(self):
         """Split data into training and testing datasets. 
         """
-        self.X_train, self.X_test, self.Y_train, self.Y_test = \
-            train_test_split(self.X, self.Y, test_size = 0.2, random_state = 42)
+        if self.data_split == None:
+            self.X_train, self.X_test, self.Y_train, self.Y_test = \
+                train_test_split(self.X, self.Y, test_size = 0.2, random_state = 24)
+        else:
+            self.X_train = self.X[np.asarray(self.data_split[0])]
+            self.X_test = self.X[np.asarray(self.data_split[1])]
+
+            self.Y_train = self.Y[np.asarray(self.data_split[0])]
+            self.Y_test = self.Y[np.asarray(self.data_split[1])]
 
     def train(self):
         """Train.
         """
+        self.split_data()
         self.model.fit(self.X_train, self.Y_train)
         self.train_predict = self.model.predict(self.X_train)
         self.test_predict = self.model.predict(self.X_test)
-        return self.model
 
-    def evaluate(self, cross_val_flag = True, print_flag = True, plot_flag = True, k = 10, metric = 'NRMSE'):
+    def evaluate(self, print_flag = True, plot_flag = True, metric = 'RMSE'):
         """Evaluate.
         Calculate RMSE score for both training and testing datasets.
         """
-        if cross_val_flag:
-            cv = ShuffleSplit(n_splits=k, test_size=0.2, random_state=42)
-            scores = cross_val_score(self.model, self.X, self.Y, cv = cv, scoring= metric)
-            scores = np.sqrt(-scores)
 
-        if metric is 'NRMSE':
+        if metric is 'RMSE':
             # use the normalised root mean square error
-            train_score = np.sqrt(mean_squared_error(self.Y_train, self.train_predict))/(max(self.Y_train) - min(self.Y_train))
-            test_score = np.sqrt(mean_squared_error(self.Y_test, self.test_predict))/(max(self.Y_test) - min(self.Y_test))
+            train_score = np.sqrt(mean_squared_error(self.Y_train, self.train_predict))
+            test_score = np.sqrt(mean_squared_error(self.Y_test, self.test_predict))
         elif metric is 'r2_score':
             train_score = r2_score(self.Y_train, self.train_predict)
             test_score =  r2_score(self.Y_test, self.test_predict)
 
         if print_flag:
             print('Model: ', str(self.model))
-            if cross_val_flag:
-                print(scores)
-                print("RMSE : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-            else:
-                print('Train '+metric+ ': '+ str(train_score))
-                print('Test '+metric+ ': '+ str(test_score))
+            print('Train '+metric+ ': '+ str(train_score))
+            print('Test '+metric+ ': '+ str(test_score))
         if plot_flag:
             self.plot()
         
@@ -121,9 +119,9 @@ class Regression():
         plt.plot(self.train_predict, self.Y_train, 'b.', label = 'train')
         max_label = max(list(self.Y_train) + list(self.Y_test))
         min_label = min(list(self.Y_train) + list(self.Y_test))
-        plt.plot([min_label,max_label], [min_label,max_label], '--')
-        plt.plot([min_label,max_label], [max_label/2.0,max_label/2.0], 'k--')
-        plt.plot([max_label/2.0,max_label/2.0], [min_label,max_label], 'k--')
+        plt.plot([min_label - 0.5,max_label+ 0.5], [min_label - 0.5,max_label+ 0.5], '--')
+        plt.plot([min_label - 0.5,max_label+ 0.5], [(max_label + min_label)/2.0,(max_label + min_label)/2.0], 'k--')
+        plt.plot([(max_label + min_label)/2.0,(max_label + min_label)/2.0], [min_label - 0.5,max_label+ 0.5], 'k--')
         
         # plt.plot([0,1], [0,1], '--')
         # plt.plot([0,1], [0.5,0.5], 'k--')
