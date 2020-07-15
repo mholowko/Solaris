0da9c34872d04162ae326355614a052561b900c5
Mengyan <u6015325@anu.edu.au>
2020-07-15 18:36:20 +1000

    generate data

--- codes/data_generating.py
+++ codes/data_generating.py
@@ -5,6 +5,15 @@
 import numpy as np
 import pandas as pd
 import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate valid data.')
+parser.add_argument('normalize_flag', default=True, help = 'indicates whether normalize labels. True for normalized label.')
+parser.add_argument('Format', default='Seq', help = 'Seq for rows as sequences; Sample for rows as samples')
+
+args = parser.parse_args()
+normalize_flag = bool(args.normalize_flag)
+data_format = args.Format
 
 sheet_name = '4h'
 Log_flag = False # indicates whether take log label
@@ -12,7 +21,7 @@ Norm_method = 'mean' # indicates how to normalize label (one of 'mean', 'minmax'
 Folder_path = os.getcwd() # folder path might need to change for different devices
 
 First_round_results_path = '/data/First_round_results/Results - First and Second Plate 3 reps.xlsx'
-
+Generated_File_Path = '/data/firstRound_' + sheet_name + '_norm' + str(normalize_flag) + '_format' + data_format + '.csv'
 def normalize(df, col_name):
     # take log FC -- possibly provide Gaussian distribution?
     if Log_flag:
@@ -33,50 +42,63 @@ def normalize(df, col_name):
 # Columns: 
 # RBS: 20-base RBS sequences
 # Name: Group and index 
-# Rep1, Rep2, Rep3: GFPOD for the 4h (using derivatives) for three biological replicates.
+# Rep1 - Rep5: GFPOD for the 4h (using derivatives) for three biological replicates.
 # AVERAGE: average value of replicates
 # STD: standard divation of replicates
 # PERC: STD/AVERAGE	
-# Group: one of 'bps', 'uni random', 'prob random', 'bandit' 
+# Group: one of 'bps', 'uni random', 'prob random', 'bandit'
+# Usable: Yes if valid; otherwise No
+# Replicates: the idx of replicates are valid to use
 
 Path_new = Folder_path + First_round_results_path
 df_new = pd.read_excel(Path_new, sheet_name= sheet_name)
-df_new['RBS'] = df_new['RBS'].str.upper() # convert to upper case
-df_new['RBS6'] = df_new['RBS'].str[7:13] # extract core part
 
-# exclude missing data (with AVERAGE > 100)
-df_new = df_new[df_new['AVERAGE'] < 100]
+# select only usable seq
+df_new_usable = df_new[df_new['Usable'] == 'Yes']
 
-# First step: normalise each Rep resectively (zero mean and unit variance)
+# select only valid replicates
+df_new_valid = pd.DataFrame()
+df_new_valid[['Name', 'Group']] = df_new_usable[['Name', 'Group']]
+df_new_valid['RBS'] = df_new_usable['RBS'].str.upper() # convert to upper case
+df_new_valid['RBS6'] = df_new_usable['RBS'].str[7:13] # extract core part
+for idx, row in df_new_usable.iterrows():
+    vad_reps = []
+    for rep_idx in row['Replicates'].split(','):
+        rep_name = 'Rep' + str(rep_idx)
+        df_new_valid.loc[idx, rep_name] = row[rep_name]
+        vad_reps.append(row[rep_name])
+    df_new_valid.loc[idx, 'AVERAGE'] = np.mean(vad_reps)
+    df_new_valid.loc[idx, 'STD'] = np.std(vad_reps)
 
-for col_name in ['Rep1', 'Rep2', 'Rep3']:
-    df_new_norm = normalize(df_new, col_name)
+# reorder columns
+df_new_valid = df_new_valid[['Name', 'Group', 'RBS', 'RBS6', 'Rep1', 'Rep2', 'Rep3', 'Rep4', 'Rep5', 'AVERAGE', 'STD']]
 
-# Second step: drop outliers, where Rep3 may have some too big labels
+# normalise each Rep respectively (zero mean and unit variance)
 
-df_new_norm['AVERAGE'] = df_new_norm.loc[: , "Rep1":"Rep3"].mean(axis=1)
-df_new_norm['STD'] = df_new_norm.loc[: , "Rep1":"Rep3"].std(axis=1)
+if normalize_flag:
+    for col_name in ['Rep1', 'Rep2', 'Rep3', 'Rep4', 'Rep5']:
+        df_new_norm = normalize(df_new_valid, col_name)
 
-outliers = df_new_norm['STD'] > 1.5
+    df_new_norm['AVERAGE'] = df_new_norm.loc[: , "Rep1":"Rep5"].mean(axis=1)
+    df_new_norm['STD'] = df_new_norm.loc[: , "Rep1":"Rep5"].std(axis=1)
+    if data_format == 'Seq':
+        df_new_norm.to_csv(Folder_path + Generated_File_Path)
+    else: # sample
+        df_new_norm_melt = pd.melt(df_new_norm, id_vars=['Name', 'RBS', 'RBS6', 'AVERAGE', 'STD', 'Group'], value_vars=['Rep2', 'Rep3', 'Rep4', 'Rep5', 'Rep1'])
+        df_new_norm_melt = df_new_norm_melt.rename(columns = {'value': 'label'})
+        df_new_norm_melt = df_new_norm_melt.dropna()
+        df_new_norm_melt.to_csv(Folder_path + Generated_File_Path)
+elif data_format == 'Seq':
+    
+    df_new_valid.to_csv(Folder_path + Generated_File_Path)
+else: # no normalise + samples
+    df_new_valid_melt = pd.melt(df_new_valid, id_vars=['Name', 'RBS', 'RBS6', 'AVERAGE', 'STD', 'Group'], value_vars=['Rep2', 'Rep3', 'Rep4', 'Rep5', 'Rep1'])
+    df_new_valid_melt = df_new_valid_melt.rename(columns = {'value': 'label'})
+    df_new_valid_melt = df_new_valid_melt.dropna()
+    df_new_valid_melt.to_csv(Folder_path + Generated_File_Path)
 
-print('Outliers:')
-print(df_new_norm[outliers])
+"""
 
-df_new_norm.at[outliers,'Rep3'] = np.nan
-
-# Third step: calculate AVERAGE and STD
-
-df_new_norm.at[outliers,'AVERAGE'] = df_new_norm.loc[outliers , "Rep1":"Rep2"].mean(axis=1)
-df_new_norm.at[outliers,'STD'] = df_new_norm.loc[outliers , "Rep1":"Rep2"].std(axis=1)
-
-df_new_norm.to_csv(Folder_path + '/data/firstRound_' + sheet_name + '.csv')
-
-# Fourth step: put each replicate as one row
-
-df_new_norm_melt = pd.melt(df_new_norm, id_vars=['RBS', 'RBS6', 'AVERAGE', 'STD', 'Group'], value_vars=['Rep1', 'Rep2', 'Rep3'])
-df_new_norm_melt = df_new_norm_melt.rename(columns = {'value': 'label'})
-
-df_new_norm_melt = df_new_norm_melt.dropna()
 #df_new_norm_melt['Group'] = 'First round result'
 
 # -----------------------------------------------------------------------------------------
@@ -107,3 +129,4 @@ all_df = df_new_norm_melt.append(df_baseline_norm)
 print(all_df)
 
 all_df.to_csv(Folder_path + '/data/firstRound_' + sheet_name + '+Baseline.csv')
+"""
