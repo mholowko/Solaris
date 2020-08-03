2639455cfd12d0c6dfbae60520ad7ff6c17f561d
Maciej Holowko <mholowko@gmail.com>
2020-08-03 15:55:42 +1000

    Updates results.

--- codes/maciek_code/ODrate.py
+++ codes/maciek_code/ODrate.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb 14 14:14:04 2020
+Created on Fri Jul 31 15:01:36 2020
 
 @author: hol428
 """
@@ -13,13 +13,15 @@ Path = directory + Filename
 
 
 df = pd.read_excel(Path,sheet_name='ODGFP')
+df2 = pd.read_excel(Path,sheet_name='OD')
 df.drop(["Time"],axis=1,inplace=True)
+df2.drop(["Time"],axis=1,inplace=True)
 
 zupa = df.min(axis=0)
 
 time = [60,120,180,240]
 
-rates = pd.DataFrame(columns=['60','120','180','240'],index = list(df.columns))
+rates = pd.DataFrame(columns=['60','120','180','240','60OD','120OD','180OD','240OD'],index = list(df.columns))
 
 
 for t in time:
@@ -29,9 +31,11 @@ for t in time:
         rate = (df.loc[start:end, [column]].sum() / t)
 
         rates.at[column,str(t)] = float(rate[0])
+        
+        rate2 = (df2.loc[start:end, [column]].sum() / t)
+
+        rates.at[column,str(t)+'OD'] = float(rate2[0])        
 
 print(rates)
 
 rates.to_csv(path_or_buf=directory+'rates.csv')
-
-    
