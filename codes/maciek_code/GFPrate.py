14d983db7f7d162e5f1f39e387f7c5ab0fa9f2e9
Maciej Holowko <mholowko@gmail.com>
2020-08-06 16:46:51 +1000

    Updated results.

--- codes/maciek_code/GFPrate.py
+++ codes/maciek_code/GFPrate.py
@@ -8,7 +8,7 @@ Created on Fri Feb 14 14:14:04 2020
 import pandas as pd
 
 directory='../../data/First_round_results/'
-Filename='Second Plate 310720 (Rep 3 of 3, full).xlsx'
+Filename='Second Plate 050820 (Rep 4 of 6, full).xlsx'
 Path = directory + Filename
 
 
