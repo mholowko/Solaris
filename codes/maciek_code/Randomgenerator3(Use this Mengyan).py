11ef2925e213ba4ef514b0362b1998147818019e
Holowko, Maciej (L&W, Eveleigh) <Maciej.Holowko@csiro.au>
2021-03-09 14:14:00 +1100

    Updated Maciek codes.

--- codes/maciek_code/Randomgenerator3(Use this Mengyan).py
+++ codes/maciek_code/Randomgenerator3(Use this Mengyan).py
@@ -9,24 +9,25 @@ import random
 import xlwt
 
 #define function that generates a stretch of DNA of n length 
-def nrandom (length=250,char=['A','T','G','C']):
+def nrandom (length=6,char=['A','T','G','C']):
     return ''.join(random.choice(['A','T','G','C']) for _ in range(length))
 
 sequences = []
 tester = []
-seqN = 30000
+seqN = 4096
 
 #sequence generator
 for seq in range (0,seqN):
     #regeneration of random sequences on each pass
     
-#    seq2 = nrandom(250)
+    seq2 = nrandom(6)
     
-#    while seq2 in tester:
-#        seq2 = nrandom(6)
+    while seq2 in tester:
+        seq2 = nrandom(6)
     
-#    tester.append(seq2)
-    sequences.append(nrandom(65) + 'TGTCTGGGTG' + nrandom(11) + 'GGCATCCA'+ nrandom(25) + 'GGCATCCA' + nrandom(8) + 'ATCCCAGCCA' + nrandom(74) + 'GTATATAAAG' + nrandom(65) + 'CACCAAG' + nrandom(62))
+    tester.append(seq2)
+    
+    sequences.append(nrandom(6))
     print('Generating sequence number: ' + str(seq))
     
 #create Excel workbook to save the sequences to
@@ -34,7 +35,7 @@ book = xlwt.Workbook()
 sh = book.add_sheet('Sequences')
 
 #generate the sequences, however many you like
-for el,row in zip(sequences,range(0,seqN)):
+for el,row in zip(tester,range(0,seqN)):
     sh.write(row,0,'Random_RBS' + '_' + str(row))
     sh.write(row,1,str(el))
 
